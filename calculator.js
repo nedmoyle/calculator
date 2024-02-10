547eb4602795863a6f6721ab54f3721867cac048
nedmoyle <140259915+nedmoyle@users.noreply.github.com>
2024-02-11 00:49:25 +1100

    created basic calculator operation functions and tested

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function add (a, b) {
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    return a/b;
+};
+
+console.log(add(2, 2));
+console.log(subtract(5, 3));
+console.log(multiply(2, 3));
+console.log(divide(12, 5));
+
